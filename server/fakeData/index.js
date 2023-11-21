e14af60fc7879c78ff73d0db842a51a8c3fc6576
tosinh <tosinh680@gmail.com>
2023-11-21 15:36:05 +0700

    first commit

---
+++ server/fakeData/index.js
@@ -0,0 +1,32 @@
+export default {
+    authors: [
+        {
+            id: 1,
+            name: 'sinh'
+        },
+        {
+            id: 2,
+            name: 'sing'
+        }
+    ],
+    folders: [
+        {
+            id: 1,
+            name: 'Home',
+            createdAt: '2023-11-17',
+            authorId: 1,
+        },
+        {
+            id: 2,
+            name: 'Home',
+            createdAt: '2023-11-17',
+            authorId: 2,
+        },
+        {
+            id: 3,
+            name: 'Home',
+            createdAt: '2023-11-17',
+            authorId: 1,
+        },
+    ]
+}
